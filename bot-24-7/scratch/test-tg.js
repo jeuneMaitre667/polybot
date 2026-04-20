50b09d136cb0709e393ac31233dd288057324041
charles <charles1508.cp@gmail.com>
2026-04-21 01:14:33 +0200

    cleanup: remove temporary diagnostic and scratch files

--- bot-24-7/scratch/test-tg.js
+++
@@ -1,9 +0,0 @@
-import { sendTelegramAlert } from './telegramAlerts.js';
-
-async function test() {
-    console.log('[Test] Sending Telegram alert...');
-    await sendTelegramAlert('🛡️🛰️⚓ TEST RECOVERY SUCCESSFUL\n\nBot: Ghost-Shield Elite v34.2\nStatus: Operational\nBalance: $134.46\nAddress: 54.247.10.200');
-    console.log('[Test] Done.');
-}
-
-test();
